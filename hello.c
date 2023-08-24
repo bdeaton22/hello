dc63e237d27eae31e1c9125b951ceefff3bab4f3
Brandon Deaton <bdeaton2@huskers.unl.edu>
2023-08-24 13:14:33 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,17 @@
-#include <stdlib.h>cd
+/**
+ * Author: Brandon Deaton
+ * Date 2023/08/24
+ *
+ * Outputs my name
+ *
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Brandon Deaton!\n");
+    printf("Computer Science\n");
+
+    return 0;
+}
